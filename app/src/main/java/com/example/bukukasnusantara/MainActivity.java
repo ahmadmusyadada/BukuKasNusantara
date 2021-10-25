8cb883debf93c92a510a5a1a711df09a61b6d3a2
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2021-10-25 19:38:37 +0700

    Change picture + update pemasukan

--- app/src/main/java/com/example/bukukasnusantara/MainActivity.java
+++ app/src/main/java/com/example/bukukasnusantara/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.bukukasnusantara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView pemasukan, pengeluaran, detail, pengaturan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pemasukan = findViewById(R.id.iv_pemasukan);
+        pengeluaran = findViewById(R.id.iv_pengeluaran);
+        detail = findViewById(R.id.iv_detail);
+        pengaturan = findViewById(R.id.iv_pengaturan);
+
+        pemasukan.setOnClickListener(new View.OnClickListener() {
+            // Start new list activity
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(MainActivity.this, PemasukanActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        pengeluaran.setOnClickListener(new View.OnClickListener() {
+            // Start new list activity
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(MainActivity.this, PengeluaranActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        detail.setOnClickListener(new View.OnClickListener() {
+            // Start new list activity
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        pengaturan.setOnClickListener(new View.OnClickListener() {
+            // Start new list activity
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(MainActivity.this, PengaturanActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bukukasnusantara/PemasukanActivity.java
+++ app/src/main/java/com/example/bukukasnusantara/PemasukanActivity.java
@@ -2,13 +2,58 @@ package com.example.bukukasnusantara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class PemasukanActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class PemasukanActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    EditText tanggal, nominal, keterangan;
+    ImageView tanggalButton;
+    Button simpan, kembali;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pemasukan);
+        tanggal = findViewById(R.id.et_date);
+        nominal = findViewById(R.id.et_nominal);
+        keterangan = findViewById(R.id.et_keterangan);
+        tanggalButton = findViewById(R.id.iv_date);
+        simpan = findViewById(R.id.btn_simpan);
+        kembali = findViewById(R.id.btn_kembali);
+
+        tanggalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+
+        tanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+    }
+
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth){
+        String date = dayOfMonth + "/" + month + "/" + year;
+        tanggal.setText(date);
     }
 }
